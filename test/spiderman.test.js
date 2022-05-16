a7f86d78fe7981280070884d7acc578432f2f6c9
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-16 14:56:18 -0500

    2. Creamos una prueba de unidad donde: Creamos un objeto de la clase spiderman con los siguientes atributos: nombre, edad, actor que lo interpreta, numero de peliculas, estudio. Corremos la prueba y observamos que falla con el siguiente error: 'ReferenceError: Spiderman is not defined'

---
+++ test/spiderman.test.js
@@ -0,0 +1,11 @@
+describe('Unit Tests for Spiderman class', () => {
+    test('1) Create an spiderman object', () => {
+        const andrewGarfield = new Spiderman('Spiderman Sony', 31, 'Andrew Garfield', 3, 'Sony');
+
+        expect(andrewGarfield.name).toBe('Spiderman Sony');
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe('Andrew Garfield');
+        expect(andrewGarfield.movies).toBe(3);
+        expect(andrewGarfield.studio).toBe('Sony');
+    });
+});
